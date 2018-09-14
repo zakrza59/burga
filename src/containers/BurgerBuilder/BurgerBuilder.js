794cd20ddb996c3d854bc93c9b0945f67ff4bd8f
zakrza59 <zakrza59@gmail.com>
2018-09-15 00:10:27 +0200

    modal animation

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -67,7 +67,7 @@ class BurgerBuilder extends Component {
     this.updatePurchaseState(updatedIngredients);
   }
 
-  purchaseHandler () {
+  purchaseHandler = () => {
     this.setState({purchasing:true});
   }
 
@@ -80,7 +80,7 @@ class BurgerBuilder extends Component {
     }
     return (
       <ReactAux>
-        <Modal>
+        <Modal show={this.state.purchasing}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
@@ -89,7 +89,8 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
           price={this.state.totalPrice}
-          purchasable={this.state.purchasable} />
+          purchasable={this.state.purchasable}
+          ordered={this.purchaseHandler} />
       </ReactAux>
     );
   }
