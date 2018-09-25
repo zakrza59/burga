08a0b722905d0de8418181646618def11284814d
zakrza59 <zakrza59@gmail.com>
2018-09-25 10:30:15 +0200

    Changed Layout to stateful component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactAux from '../../hoc/ReactAux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
-const Layout = (props) => (
-  <ReactAux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </ReactAux>
-);
+class Layout extends Component {
+  render() {
+    return (
+      <ReactAux>
+        <Toolbar />
+        <SideDrawer />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </ReactAux>
+    )
+  }
+}
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import ReactAuth from '../../../hoc/ReactAux';
+
 import classes from './SideDrawer.css';
+import ReactAux from '../../../hoc/ReactAux';
 
 const SideDrawer = (props) => {
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <ReactAux>
+      <Backdrop show/>
+      <div className={classes.SideDrawer}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </ReactAux>
   )
 }
 
