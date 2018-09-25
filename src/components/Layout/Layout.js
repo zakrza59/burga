24f52ccb40946979e45d96d332aa876ee1b0cac4
zakrza59 <zakrza59@gmail.com>
2018-09-25 10:42:40 +0200

    Closing sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,11 +5,21 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
 class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  }
+
   render() {
     return (
       <ReactAux>
         <Toolbar />
-        <SideDrawer />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler} />
         <main className={classes.Content}>
           {this.props.children}
         </main>
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import ReactAuth from '../../../hoc/ReactAux';
-
-import classes from './SideDrawer.css';
 import ReactAux from '../../../hoc/ReactAux';
+import classes from './SideDrawer.css';
 
 const SideDrawer = (props) => {
   return (
     <ReactAux>
-      <Backdrop show/>
+      <Backdrop show={props.open} clicked={props.closed} />
       <div className={classes.SideDrawer}>
         <div className={classes.Logo}>
           <Logo />
