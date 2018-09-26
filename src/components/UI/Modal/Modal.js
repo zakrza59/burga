64ed15ed098fb4a983f36748200522b9cffd91b5
zakrza59 <zakrza59@gmail.com>
2018-09-26 22:11:11 +0200

    fix

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,6 +5,11 @@ import Backdrop from '../Backdrop/Backdrop';
 import classes from './Modal.css'
 
 class Modal extends Component {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
   render() {
     return (
       <ReactAux>
