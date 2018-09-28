4109ec53bb252a4384205e9ebb732775c231ae0b
zakrza59 <zakrza59@gmail.com>
2018-09-29 00:34:37 +0200

    Addede spinner

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -76,7 +77,25 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!');
+    // alert('You continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Wixa Cziken',
+        address: {
+          street: 'Nibyczegolada 1',
+          zipCode: '9-912',
+          country: 'PRLAND'
+        },
+        email: 'donotsendhere@g.com'
+      },
+      deliveryMethod: 'uberEats'
+    }
+
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
 
 
