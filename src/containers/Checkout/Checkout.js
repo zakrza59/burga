1cc99371942121f8ec3aaae55e0f2e03d2c42590
zakrza59 <zakrza59@gmail.com>
2018-10-02 10:59:51 +0200

    Added contact to checkout

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,40 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import { Route } from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
- state = {
-   ingredients: {
-     salad: 1,
-     meat: 1,
-     bacon: 1,
-     cheese: 1
-   }
- }
-
-componentDidMount() {
-  const query = new URLSearchParams(this.props.location.search)
-  const ingredients = {};
-  for (let param of query.entries()) {
-    ingredients[param[0]] = +param[1];
+  state = {
+    ingredients: {
+      salad: 1,
+      meat: 1,
+      bacon: 1,
+      cheese: 1
+    }
   }
-  this.setState({ingredients: ingredients})
-}
 
- checkoutCancelledHandler = () => {
-   this.props.history.goBack();
- }
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search)
+    const ingredients = {};
+    for (let param of query.entries()) {
+      ingredients[param[0]] = +param[1];
+    }
+    this.setState({ ingredients: ingredients })
+  }
 
- checkoutContinuedHandler = () => {
-  this.props.history.replace('/checkout/contact-data');
-}
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  }
 
-  render () {
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data');
+  }
+
+  render() {
     return (
       <div>
-        <CheckoutSummary 
-        ingredients={this.state.ingredients}
-        checkoutContinued={this.checkoutContinuedHandler}
-        checkoutCancelled={this.checkoutCancelledHandler}/>        
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutContinued={this.checkoutContinuedHandler}
+          checkoutCancelled={this.checkoutCancelledHandler} />
+        <Route
+          path={this.props.match.path + '/contact-data'}
+          component={ContactData} />
       </div>
     )
   }
