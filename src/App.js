da831ee9cfa0992e0269cba0355e5c8197355b7c
zakrza59 <zakrza59@gmail.com>
2018-10-02 20:05:14 +0200

    added orders to app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/" exact component={BurgerBuilder} />
+            <Route path="/orders" exact component={Orders} />
             <Route path="/checkout" component={Checkout} />
           </Switch>
         </Layout>
