e64046c72136cdd10605e624e0770cb644a009e4
zakrza59 <zakrza59@gmail.com>
2018-10-02 20:07:36 +0200

    Fixed nav

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import classes from './NavigationItem.css';
-
+import { NavLink } from 'react-router-dom';
 
 const NavigationItem = (props) => (
   <li className={classes.NavigationItem}>
-    <a
-      href={props.link}
-      className={props.active ? classes.active : null}>{props.children}</a>
+    <NavLink
+      to={props.link}>{props.children}</NavLink>
   </li>
 )
 export default NavigationItem;
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,7 @@ import classes from './NavigationItems.css';
 const NavigationItems = () => (
   <ul className={classes.NavigationItems}>
     <NavigationItem link="/" active>Burger Builder</NavigationItem>
-    <NavigationItem link="/">Checkout</NavigationItem>
+    <NavigationItem link="/orders">Orders</NavigationItem>
   </ul>  
 )
 export default NavigationItems;
