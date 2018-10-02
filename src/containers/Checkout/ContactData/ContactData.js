a6384d7605af7a7c5aaf49ee543e053fff3b9340
zakrza59 <zakrza59@gmail.com>
2018-10-02 11:03:55 +0200

    Css for inputs

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -17,10 +17,10 @@ class ContactData extends Component {
       <div className={classes.ContactData}>
         <h4>Entry your Contact Data</h4>
         <form>
-          <input type="text" name="name" placeholder="Your Name"/>
-          <input type="email" name="email" placeholder="Your Mail"/>
-          <input type="text" name="street" placeholder="Your street"/>
-          <input type="text" name="postal" placeholder="Postal Code"/>
+          <input className={classes.Input} type="text" name="name" placeholder="Your Name"/>
+          <input className={classes.Input} type="email" name="email" placeholder="Your Mail"/>
+          <input className={classes.Input} type="text" name="street" placeholder="Your street"/>
+          <input className={classes.Input} type="text" name="postal" placeholder="Postal Code"/>
           <Button btnType="Success">ORDER</Button>
         </form>
       </div>
