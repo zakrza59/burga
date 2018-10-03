3b8f8e82442c099db12b96a017866b262e36e373
zakrza59 <zakrza59@gmail.com>
2018-10-03 22:59:02 +0200

    addded switch to input

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const Input = (props) => (
+const Input = (props) => {
+  let inputElement = null;
 
-)
+  switch (props.inputType) {
+    case ('input'):
+      inputElement = <input {...props} />;
+      break;
+    case ('textarea'):
+      inputElement = <textarea {...props} />;
+      break;
+    default:
+      inputElement = <input {...props} />;
+  }
+
+  return (
+    <div>
+      <label>{props.label}</label>
+      {inputElement}
+    </div>
+  )
+}
 
 export default Input;
