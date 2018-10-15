38755b5ba8030168bf32ce4880ec0f1db03efbef
zakrza59 <zakrza59@gmail.com>
2018-10-15 16:50:24 +0200

    switch in reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -14,11 +14,19 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
       return {
-
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+        }
       };
     case actionTypes.REMOVE_INGREDIENT:
       return {
-
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+        }
       };
     default:
       return state;
