ffb0ff3a370016424f9dd6345fc37a69db5b961d
zakrza59 <zakrza59@gmail.com>
2018-10-16 12:09:20 +0200

    order now works

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,6 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false
@@ -37,7 +36,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({ purchasable: sum > 0 })
+   return sum > 0;
   }
 
   purchaseHandler = () => {
@@ -83,9 +82,9 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            price={this.props.price}
-            purchasable={this.state.purchasable}
-            ordered={this.purchaseHandler} />
+            purchasable={this.updatePurchaseState(this.props.ings)}
+            ordered={this.purchaseHandler}
+            price={this.props.price} />
         </ReactAux>
       );
       orderSummary = <OrderSummary
