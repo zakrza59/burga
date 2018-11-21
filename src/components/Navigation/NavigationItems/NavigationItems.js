07b39e9327d1816713ab3fd6632ef25d6ae29910
zakrza59 <zakrza59@gmail.com>
2018-11-21 14:24:22 +0100

    Navbar auth

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,11 +2,14 @@ import React from 'react';
 import NavigationItem from './NavigationItem/NavigationItem';
 import classes from './NavigationItems.css';
 
-const NavigationItems = () => (
+const NavigationItems = (props) => (
   <ul className={classes.NavigationItems}>
     <NavigationItem link="/" exact>Burger Builder</NavigationItem>
     <NavigationItem link="/orders">Orders</NavigationItem>
-    <NavigationItem link="/auth">Authenticate</NavigationItem>
+    {!props.isAuthenticated
+      ? <NavigationItem link="/auth">Authenticate</NavigationItem>
+      : <NavigationItem link="/logout">Logout</NavigationItem>
+    }
   </ul>  
 )
 export default NavigationItems;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import ReactAux from '../ReactAux/ReactAux';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
@@ -22,8 +24,11 @@ class Layout extends Component {
   render() {
     return (
       <ReactAux>
-        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
+          drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer
+          isAuth={this.props.isAuthenticated}
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler} />
         <main className={classes.Content}>
@@ -34,4 +39,10 @@ class Layout extends Component {
   }
 }
 
-export default Layout;
+const mapStateToProps = state => {
+  return {
+      isAuthenticated: state.auth.token !== null
+  };
+};
+
+export default connect(mapStateToProps)(Layout);
